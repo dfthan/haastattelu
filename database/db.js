8d221578119e40fc89fcf7da6ad0f5154958ff53
Hannu <varahahmo1@gmail.com>
2023-01-27 01:00:02 +0200

    mongoDB added

---
+++ database/db.js
@@ -0,0 +1,14 @@
+const { MONGODB_URI } = require("../lib/config")
+const mongoose = require("mongoose")
+
+function connectDB() {
+    try {
+        mongoose.connect(MONGODB_URI)
+        console.log("Connected to mongoDB")
+    } catch (error) {
+        console.error(error)
+    }
+
+}
+
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
-const { PORT } = require('./util/config')
+const { PORT } = require('./lib/config')
+const connectDB = require('./database/db.js')
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(bodyParser.json())
+connectDB()
+
+app.use("/customers", require("./controllers/customers"))
 
 app.listen(PORT, () => {
   console.log(`App listening on ${PORT}`)
